5c7252fb0c83cb52f744ec655b2c5d86c05b9211
Sandy Gomez <sgomez1029@gmail.com>
2023-06-20 14:21:16 -0700

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Sandra Gomez on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
